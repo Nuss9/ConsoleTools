ac3526036fed0b8cf0182c7062f2422b38e41a7d
Dennis Jakupovic <djakupovic@winbase.nl>
2019-12-17 15:03:30 +0100

    Add Solution and Projects

---
+++ RenamerTests/FileBatchRenamerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RenamerTests
+{
+    public class FileBatchRenamerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
